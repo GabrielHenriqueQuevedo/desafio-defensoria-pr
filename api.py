40c2a5a4e7760f781078fd4fa3a460e5562f0a3c
Gabriel <50281898+GabrielHenriqueQuevedo@users.noreply.github.com>
2026-05-30 15:18:32 -0300

    Added comments and configured the documentation

--- api.py
+++ api.py
@@ -2,9 +2,20 @@ import requests
 from fastapi import FastAPI
 from datetime import date
 
-app: FastAPI = FastAPI()
+# Cria um objeto do tipo "FastApi" e configura a documentação
+app: FastAPI = FastAPI(
+    title = "Desafio Defensoria PR",
+    description = "API de consumação de uma API externa de preços de tapetes.",
+    version = "1.0.0",
+    redoc_url = None
+)
 
-@app.get("/rugs")
+# Cria o endpoint "/rugs" e o coloca na tag "Endpoint" do swagger
+@app.get("/rugs", tags=["Endpoint"])
+
+# Cria a função executada ao acessar o endpoint "/rugs"
+# Função consome uma API externa, caso tenha sucesso retorna um dicionário contendo a data atual
+# Junto com as informações dos tapetes
 async def get_rugs(date: date = date.today()) -> dict:
     url: str = "https://testedefensoriapr.pythonanywhere.com/precos"
     response: requests.models.Response = requests.get(url)
